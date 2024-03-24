a28230925e62bf12cec36bd0f2350ab8a4bb42f5
Antoha <neverov12@gmail.com>
2024-03-24 16:04:09 +0300

    вишлист обновил

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -23,43 +23,6 @@ const items: WishlistItemData[] = [
         ]
     },
     {
-        name: 'Твердый шампунь «Это я»',
-        price: 650,
-        image: '/wishlist/soap2.jpg',
-        text: 'Вкусно пахнет и хорошо моет. Хватает только совсем не надолго. Нравится с лавандой, но интересно попробовать и другие ароматы',
-        links: [
-            { name: 'Ozon', url: 'https://www.ozon.ru/product/eto-ya-tvoy-tverdyy-shampun-s-lavandoy-218644052' },
-        ]
-    },
-    {
-        name: 'Средство De\'Longhi EcoDecalk DLSC500',
-        price: 1000,
-        image: '/wishlist/eco.jpg',
-        text: 'Всё для той же кофемашины. Это средство требуется для промывки примерно раз в месяц. Одной банки хватает на 5 промывок.',
-        links: [
-            { name: 'Яндекс.Маркет', url: 'https://market.yandex.ru/product--sredstvo-de-longhi-ecodecalk-dlsc500-500-ml/388454715' },
-            { name: 'De\'Longhi', url: 'https://delonghi.ru/product/de-longhi-sr-ochistki-ot-nakipi-de-longhisc500' },
-        ]
-    },
-    {
-        name: 'Трусы',
-        price: 600,
-        image: '/wishlist/pantsu.png',
-        text: 'Трусы только бренда Uniqlo размера XL (58 RUS). Заказывать можно только с Ламоды, больше нигде не продаются.',
-        links: [
-            { name: 'Lamoda - ссылка на модели, которые точно подойдут', url: 'https://www.lamoda.ru/c/5291/clothes-bokserymuj/?sitelink=breadcrumbs&brands=33366&size_values=58' },
-        ]
-    },
-    {
-        name: 'Ящик молока Parmalat 0.05% без лактозы',
-        price: 1500,
-        image: '/wishlist/milk.jpg',
-        text: 'Пью молоко каждый день с кофе. На неделю хватает двух пачек.',
-        links: [
-            { name: 'Ozon', url: 'https://www.ozon.ru/product/moloko-parmalat-uht-comfort-bezlaktoznoe-0-05-1-l-h-12-sht-178324381' },
-        ]
-    },
-    {
         name: 'Фисташки',
         price: 1600,
         image: '/wishlist/fistashki.jpg',
@@ -69,11 +32,52 @@ const items: WishlistItemData[] = [
         ]
     },
     {
+        name: 'Ящик молока Parmalat 0.05% без лактозы',
+        price: 1700,
+        image: '/wishlist/milk.jpg',
+        text: 'Пью молоко каждый день с кофе. На неделю хватает одной пачки.',
+        links: [
+            { name: 'Ozon', url: 'https://www.ozon.ru/product/moloko-parmalat-uht-comfort-bezlaktoznoe-0-05-1-l-h-12-sht-178324381' },
+        ]
+    },
+    {
+        name: 'Ящик сладких напитков',
+        price: 2500,
+        image: '/wishlist/cola.png',
+        text: 'Люблю всякие напитки. Чтобы не дарить чуть-чуть, можно подарить сразу ящик! Я люблю: Coca-Cola (Добрый кола) обычная/ванильная/без сахара, капучино/латте от Lotte/Parmalat, Милкис обычный, Chupa Chups, Dr. Pepper (обычный, Cream Soda), Dr.Pepper Flat Vanilla (мой любимый)',
+        links: [
+            { name: 'Добрый, Ozon', url: 'https://www.ozon.ru/product/gazirovannyy-napitok-dobryy-kola-24-sht-h-0-33-l-688376387/' },
+            { name: 'Добрый без сахара, Ozon', url: 'https://www.ozon.ru/product/gazirovannyy-napitok-dobryy-kola-bez-sahara-12-sht-h-0-33-l-688375923' },
+            { name: 'Dr Pepper, Ozon', url: 'https://www.ozon.ru/product/napitok-gazirovannyy-dr-pepper-classic-24-sht-h-330-ml-doktor-pepper-1215351614' },
+            { name: 'Chupa-Chups клубинчный, Ozon', url: 'https://www.ozon.ru/product/chupa-chups-klubnika-so-slivkami-0-345l-12sht-chupa-chups-1418772607' },
+        ]
+    },
+    {
+        name: 'Корейские/Японские сладости',
+        price: 2500,
+        image: '/wishlist/candy.png',
+        text: 'Любые интересные корейские или японские сладости. Продаются даже наборами. А можно купить и по-отдельности.',
+        links: [
+            { name: 'Набор от JCANDY, Ozon', url: 'https://www.ozon.ru/product/sladkiy-podarok-jcandy-prime-box-premium-edition-yaponskie-koreyskie-aziatskie-sladosti-1194574600' },
+        ]
+    },
+    {
+        name: 'Зарядная станция для геймпадов PS5',
+        price: 2500,
+        image: '/wishlist/dock.png',
+        text: 'ВАЖНО! Очень часто подделывают. Нужно читать отзывы или покупать в проверенных магазина (DNS, М.Видео, Эльдорадо). Ссылки на оригиналы добавил. Если цена ниже - скорее всего, подделка. На озоне не нашёл дешёвых оригинальных вариантов.',
+        links: [
+            { name: 'Яндекс.Маркет', url: 'https://market.yandex.ru/product--zariadnaia-stantsiia-dualsense-na-dva-geimpada-dlia-ps5' },
+            { name: 'DNS', url: 'https://www.dns-shop.ru/product/ea7634350414ed20/zaradnaa-stancia-playstation-dualsense-charging-station-belyj' },
+        ]
+    },
+    {
         name: 'Игры для PlayStation 5',
         price: 5000,
         image: '/wishlist/games.jpg',
         text: 'Хороших игр много, многие из них я пропустил, нужно наращивать упущенное. Чтобы не удлинять список, здесь оставляю ссылку на все игры, которые мне интересны (сверху самые интересные, ниже менее интересные). Купить можно где угодно. Важно, чтобы продавец продавал именно диск с игрой, а не аккаунт или цифровую версию.',
         links: [
+            { name: 'Dragons Dogma 2 - 7500 руб, Ozon', url: 'https://www.ozon.ru/product/igra-dragons-dogma-ii-2-playstation-5-russkie-subtitry-1459408726' },
             { name: 'Hogwarts Legacy - 5500 руб, Маркет', url: 'https://market.yandex.ru/product--igra-hogwarts-legacy-standard-edition-dlia-playstation-5/1812470565' },
             { name: 'Ratchet & Clank: Сквозь Миры - 4000 руб, Маркет', url: 'https://market.yandex.ru/product--igra-ratchet-clank-skvoz-miry/942818431' },
         ]
@@ -98,36 +102,36 @@ const items: WishlistItemData[] = [
         name: 'Роутер Keenetic Hopper',
         price: 7000,
         image: '/wishlist/router.jpg',
-        text: 'Давно хочу обновить роутер. Но мой меня вполне устраивает. Но вот бы wifi 6 и USB порт не помешал бы совсем. Keenetic делает лучшие роутеры на данный момент. Hopper - минимально оптимальная модель, отвечающая всем моим требованиям. Можно и круче, например Giga.',
+        text: 'Давно хочу обновить роутер. Но мой меня вполне устраивает. Но вот wifi 6 и USB порт не помешали бы совсем. Keenetic делает лучшие роутеры на данный момент. Hopper - минимально оптимальная модель, отвечающая всем моим требованиям. Можно и круче, например Giga.',
         links: [
             { name: 'Ozon', url: 'https://www.ozon.ru/product/wi-fi-router-keenetic-hopper-kn-3810-554543961' },
         ]
     },
     {
-        name: 'Курсы английского',
-        price: 10000,
-        image: '/wishlist/eng.png',
-        text: 'В школах Инглекс или Яндекс.Практикум',
+        name: 'Белая Яндекс.Станция Миди с Zigbee',
+        price: 12000,
+        image: '/wishlist/ya.png',
+        text: 'Zigbee - это такой протокол для управлением умным домом',
         links: [
-            { name: 'Ozon', url: 'https://www.ozon.ru/product/wi-fi-router-keenetic-hopper-kn-3810-554543961' },
+            { name: 'Ozon', url: 'https://www.ozon.ru/product/umnaya-kolonka-yandeks-stantsiya-midi-s-alisoy-s-zigbee-seryy-1406852214' },
+            { name: 'Яндекс.Маркет', url: 'https://market.yandex.ru/product--stantsiia-midi/1910792707?sku=102224829824' },
         ]
     },
     {
-        name: 'Геймпад DualSense Edge',
-        price: 24000,
-        image: '/wishlist/gamepad.jpg',
-        text: 'Самый крутой геймпад на данный момент. Но и стоит как половина консоли.',
+        name: 'Galaxy Watch 6 Classic 43mm',
+        price: 25000,
+        image: '/wishlist/watch.png',
+        text: 'Чёрные или серебристые часы с вензелем, 43мм. Бывает ещё более крутая версия на 47мм, иногда она даже дешевле младшей версии.',
         links: [
-            { name: 'CDEK Shopping', url: 'https://cdek.shopping/p/353106/besprovodnoi-geimpad-sony-dualsense-edge-belyi' },
-            { name: 'ИгроРай', url: 'https://igroray.ru/catalog/geympad-sony-dualsense-edge/' },
-            { name: 'Яндекс.Маркет', url: 'https://market.yandex.ru/product--besprovodnoi-geimpad-dualsense-edge-cfi-zcp1/1810726144' },
+            { name: 'Серебристые, Яндекс.Маркет', url: 'https://market.yandex.ru/product--samsung-galaxy-watch-6-classic-43mm-silver/1934571910?sku=102399066883&do-waremd5=PDLByR3dnqxNbuNzaGvtTQ&uniqueId=965815' },
+            { name: 'Черные, Ozon', url: 'https://www.ozon.ru/product/samsung-umnye-chasy-galaxy-watch-6-classic-47mm-r960-versiya-version-1-5-super-amoled-ekran-gps-1176165742/' },
         ]
     },
     {
         name: 'Nissan Qashqai',
         price: 2500000,
         image: '/wishlist/car.png',
-        text: 'Ну тут всё понятно. Можно, конечно и X-Trail, отказываться не буду',
+        text: 'Можно, конечно и X-Trail, отказываться не буду',
     },
 ];
 
